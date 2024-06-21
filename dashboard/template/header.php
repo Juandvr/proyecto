a876dd63b9accc4429f7d60ee1d0c0db6ac4117b
Juandvr <villabonajuan203@gmail.com>
2024-06-20 22:29:42 -0500

    Se agregaron cambios al header

--- dashboard/template/header.php
+++ dashboard/template/header.php
@@ -14,7 +14,7 @@
         <aside class="sidebar">
             <h2 class="logo">Dashboard</h2>
             <nav class="menu">
-                <a href="../index.php">Volver al inicio</a>
+                <a href="../index.php"><i class="material-icons" style="font-size: 14px;">arrow_back</i> Volver al inicio</a>
             </nav>
             <nav class="menu">
                 <a href="index.php">Usuarios</a>
@@ -32,10 +32,26 @@
                 <a href="citas/index_citas.php">Citas</a>
             </nav>
         </aside>
+        <?php 
+        session_start();
+        
+        include '../db_connection.php';
+        
+        // Obtener información del usuario
+        $usuario_id = $_SESSION['usuario_id'];
+        $sql = "SELECT * FROM cliente WHERE ID_Cliente = '$usuario_id'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+          $usuario = $result->fetch_assoc();
+        } else {
+          echo "Error al obtener los datos del usuario.";
+          exit();
+        }
+        ?>
         <main class="main-content">
             <header class="header">
                 <div class="user-info">
-                    <span class="user-name">Administrador</span>
+                    <span class="user-name">Bienvenido, <?php echo $usuario['nombre']?></span>
                     <i class="material-icons">account_box</i>
                 </div>
             </header>
